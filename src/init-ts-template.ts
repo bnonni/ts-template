3b21527f0e3bc027dfb2c1c48dae3a43b1c7e562
bnonni <github@nonni.dev>
2025-02-18 15:36:16 -0500

    version bump 0.2.1

--- src/init-ts-template.ts
+++ src/init-ts-template.ts
@@ -4,7 +4,7 @@ import TsTemplate from './ts-template.js';
 
 // cli: init-ts-template
 program
-  .version('init-ts-template 0.1.0', '-v, --version', 'Output the current version.')
+  .version('init-ts-template 0.2.1', '-v, --version', 'Output the current version.')
   .description('TypeScript project template initializer with support for tsconfig.')
   .option('-n, --name <name>', 'Package name.')
   .action(async (options: any) => await TsTemplate.init(options))
