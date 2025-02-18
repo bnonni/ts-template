cf9477bb5b12277fa7cca1e9d2e3d5f761b1789f
bnonni <github@nonni.dev>
2025-02-18 15:58:05 -0500

    fix versioning

--- src/init-ts-template.ts
+++ src/init-ts-template.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import { program } from 'commander';
 import TsTemplate from './ts-template.js';
-
+const pkg = require('../../package.json');
 // cli: init-ts-template
 program
-  .version('init-ts-template 0.2.2', '-v, --version', 'Output the current version.')
+  .version(`init-ts-template ${pkg.version}`, '-v, --version', 'Output the current version.')
   .description('TypeScript project template initializer with support for tsconfig.')
   .option('-n, --name <name>', 'Package name.')
   .action(async (options: any) => await TsTemplate.init(options))
