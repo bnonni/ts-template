b39d2f3eccc8fe43d173138ba45d1fedc98b16e7
bnonni <github@nonni.dev>
2025-02-19 16:23:30 -0500

    remove conflicting version

--- src/cli.ts
+++ src/cli.ts
@@ -7,7 +7,6 @@ program
   .version(`init-ts-template ${pkg.version}`, '-v, --version', 'Output the current version.')
   .description('TypeScript project template initializer with support for tsconfig.')
   .option('-n, --name <name>', 'Package name.')
-  .option('-v, --version <version>', 'Package version.')
   .option('-d, --description <description>', 'Package description.')
   .action(async (options: any) => await TsTemplate.init(options))
   .parse();
